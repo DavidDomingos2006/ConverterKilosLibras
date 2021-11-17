5a889750dcbb3bf4a8b2613e59910f1ea213017c
daviddomingos2006 <daviddomingos2006@hotmail.com>
2021-11-17 12:21:48 -0300

    Criando o Projeto

---
+++ src/ConverterKilosLibras/Principal.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConverterKilosLibras;
+import java.util.Scanner;
+/**
+ *
+ * @author david
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Scanner tec = new Scanner(System.in);
+        
+        //1 libra tem 450g
+        //1000 gramas == 1 kg
+        System.out.println(converterLibraEmKg(2));
+        System.out.println(converterKgEmLibra(10));
+        
+    }
+    
+    public static double converterLibraEmKg(double valor){
+        double libra = 0.450;
+        
+        //libra convertida em Kilograma
+        double total = valor * libra;
+    
+        return total;
+    }
+    
+    public static double converterKgEmLibra(double valor2){
+        double libra = 0.450;
+        
+        double total = valor2 / libra;
+        
+        return total;
+    }
+    
+}
